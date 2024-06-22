4fa08fa84a3c85351f57343f4676e9bf5147cb18
Kavindu Nirmal <65773645+KavinduNirmal@users.noreply.github.com>
2024-06-22 19:41:26 +0530

    Landing Page Design

--- pocketify/pocketify/LandingForm.Designer.cs
+++ pocketify/pocketify/LandingForm.Designer.cs
@@ -29,7 +29,104 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LandingForm));
+            this.login_signup_btn = new System.Windows.Forms.Button();
+            this.login_login_btn = new System.Windows.Forms.Button();
+            this.login_pw_label = new System.Windows.Forms.Label();
+            this.login_pw_inp = new System.Windows.Forms.TextBox();
+            this.login_username_label = new System.Windows.Forms.Label();
+            this.login_un_inp = new System.Windows.Forms.TextBox();
+            this.login_logo_1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.login_logo_1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // login_signup_btn
+            // 
+            this.login_signup_btn.BackColor = System.Drawing.Color.Transparent;
+            this.login_signup_btn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(65)))));
+            this.login_signup_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.login_signup_btn.Font = new System.Drawing.Font("Inter Medium", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_signup_btn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(65)))));
+            this.login_signup_btn.Location = new System.Drawing.Point(650, 355);
+            this.login_signup_btn.Name = "login_signup_btn";
+            this.login_signup_btn.Size = new System.Drawing.Size(140, 39);
+            this.login_signup_btn.TabIndex = 13;
+            this.login_signup_btn.Text = "Sign Up";
+            this.login_signup_btn.UseVisualStyleBackColor = false;
+            // 
+            // login_login_btn
+            // 
+            this.login_login_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(133)))), ((int)(((byte)(231)))));
+            this.login_login_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.login_login_btn.Cursor = System.Windows.Forms.Cursors.Default;
+            this.login_login_btn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(84)))), ((int)(((byte)(191)))));
+            this.login_login_btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(84)))), ((int)(((byte)(191)))));
+            this.login_login_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(84)))), ((int)(((byte)(191)))));
+            this.login_login_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.login_login_btn.Font = new System.Drawing.Font("Inter Medium", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_login_btn.ForeColor = System.Drawing.Color.White;
+            this.login_login_btn.Location = new System.Drawing.Point(490, 355);
+            this.login_login_btn.Name = "login_login_btn";
+            this.login_login_btn.Size = new System.Drawing.Size(140, 39);
+            this.login_login_btn.TabIndex = 12;
+            this.login_login_btn.Text = "Login";
+            this.login_login_btn.UseVisualStyleBackColor = false;
+            // 
+            // login_pw_label
+            // 
+            this.login_pw_label.AutoSize = true;
+            this.login_pw_label.BackColor = System.Drawing.Color.Transparent;
+            this.login_pw_label.Font = new System.Drawing.Font("Inter Medium", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_pw_label.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.login_pw_label.Location = new System.Drawing.Point(394, 299);
+            this.login_pw_label.Name = "login_pw_label";
+            this.login_pw_label.Size = new System.Drawing.Size(90, 20);
+            this.login_pw_label.TabIndex = 11;
+            this.login_pw_label.Text = "Password";
+            this.login_pw_label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // login_pw_inp
+            // 
+            this.login_pw_inp.Font = new System.Drawing.Font("Inter", 10F);
+            this.login_pw_inp.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.login_pw_inp.Location = new System.Drawing.Point(490, 296);
+            this.login_pw_inp.MaxLength = 16;
+            this.login_pw_inp.Name = "login_pw_inp";
+            this.login_pw_inp.PasswordChar = '•';
+            this.login_pw_inp.Size = new System.Drawing.Size(300, 28);
+            this.login_pw_inp.TabIndex = 10;
+            // 
+            // login_username_label
+            // 
+            this.login_username_label.AutoSize = true;
+            this.login_username_label.BackColor = System.Drawing.Color.Transparent;
+            this.login_username_label.Font = new System.Drawing.Font("Inter Medium", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_username_label.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.login_username_label.Location = new System.Drawing.Point(391, 240);
+            this.login_username_label.Name = "login_username_label";
+            this.login_username_label.Size = new System.Drawing.Size(93, 20);
+            this.login_username_label.TabIndex = 9;
+            this.login_username_label.Text = "Username";
+            this.login_username_label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // login_un_inp
+            // 
+            this.login_un_inp.Font = new System.Drawing.Font("Inter", 10F);
+            this.login_un_inp.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.login_un_inp.Location = new System.Drawing.Point(490, 237);
+            this.login_un_inp.MaxLength = 20;
+            this.login_un_inp.Name = "login_un_inp";
+            this.login_un_inp.Size = new System.Drawing.Size(300, 28);
+            this.login_un_inp.TabIndex = 8;
+            // 
+            // login_logo_1
+            // 
+            this.login_logo_1.Image = global::pocketify.Properties.Resources.pocketify_logo;
+            this.login_logo_1.Location = new System.Drawing.Point(585, 169);
+            this.login_logo_1.Name = "login_logo_1";
+            this.login_logo_1.Size = new System.Drawing.Size(111, 24);
+            this.login_logo_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.login_logo_1.TabIndex = 7;
+            this.login_logo_1.TabStop = false;
             // 
             // LandingForm
             // 
@@ -39,18 +136,35 @@
             this.BackgroundImage = global::pocketify.Properties.Resources.loginBG;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1262, 753);
+            this.Controls.Add(this.login_signup_btn);
+            this.Controls.Add(this.login_login_btn);
+            this.Controls.Add(this.login_pw_label);
+            this.Controls.Add(this.login_pw_inp);
+            this.Controls.Add(this.login_username_label);
+            this.Controls.Add(this.login_un_inp);
+            this.Controls.Add(this.login_logo_1);
             this.DoubleBuffered = true;
-            this.ForeColor = System.Drawing.Color.Black;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "LandingForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Pocketify – Login";
+            ((System.ComponentModel.ISupportInitialize)(this.login_logo_1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button login_signup_btn;
+        private System.Windows.Forms.Button login_login_btn;
+        private System.Windows.Forms.Label login_pw_label;
+        private System.Windows.Forms.TextBox login_pw_inp;
+        private System.Windows.Forms.Label login_username_label;
+        private System.Windows.Forms.TextBox login_un_inp;
+        private System.Windows.Forms.PictureBox login_logo_1;
     }
 }
 
--- pocketify/pocketify/Properties/Resources.Designer.cs
+++ pocketify/pocketify/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace pocketify.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pocketify_logo {
+            get {
+                object obj = ResourceManager.GetObject("pocketify-logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
