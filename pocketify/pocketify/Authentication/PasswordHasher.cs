080e95719de3cc0f66c9af6439921d1f28e78dca
Kavindu Nirmal <65773645+KavinduNirmal@users.noreply.github.com>
2024-06-22 19:56:12 +0530

    Created PasswordHasher

--- pocketify/pocketify/Authentication/PasswordHasher.cs
+++ pocketify/pocketify/Authentication/PasswordHasher.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace pocketify.Authentication
 {
     internal class PasswordHasher
     {
+        public static string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var salt = GenerateSalt();
+                var saltedPassword = password + salt;
+                var hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(saltedPassword));
+                return Convert.ToBase64String(hash) + ":" + salt;
+            }
+        }
+
+        public static bool VerifyPassword(string enteredPassword, string storedHash)
+        {
+            var parts = storedHash.Split(':');
+            var hash = parts[0];
+            var salt = parts[1];
+            using (var sha256 = SHA256.Create())
+            {
+                var saltedPassword = enteredPassword + salt;
+                var enteredHash = sha256.ComputeHash(Encoding.UTF8.GetBytes(saltedPassword));
+                return Convert.ToBase64String(enteredHash) == hash;
+            }
+        }
+
+        private static string GenerateSalt()
+        {
+            var buffer = new byte[16];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(buffer);
+            }
+            return Convert.ToBase64String(buffer);
+        }
     }
 }
