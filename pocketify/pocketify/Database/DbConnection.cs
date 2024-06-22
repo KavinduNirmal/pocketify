718f4ba29934684e7e639aa84090c8a8d3025ba6
Kavindu Nirmal <65773645+KavinduNirmal@users.noreply.github.com>
2024-06-22 20:14:31 +0530

    Added Connection string

--- pocketify/pocketify/Database/DbConnection.cs
+++ pocketify/pocketify/Database/DbConnection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,17 @@ namespace pocketify.Database
 {
     internal class DbConnection
     {
+        private readonly string _connectionString;
+
+        public DbConnection()
+        {
+            // TODO: Create an environment variable instead.
+            _connectionString = "Data Source=MSI;Initial Catalog=Pocketify;Integrated Security=True";
+        }
+
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
     }
 }
