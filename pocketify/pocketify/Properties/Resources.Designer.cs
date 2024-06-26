79f02c860a0ca7aced4ab29ffc3c2cd584d8fa9d
Kavindu Nirmal <65773645+KavinduNirmal@users.noreply.github.com>
2024-06-26 15:04:26 +0530

    Updated

--- pocketify/pocketify/Properties/Resources.Designer.cs
+++ pocketify/pocketify/Properties/Resources.Designer.cs
@@ -163,6 +163,16 @@ namespace pocketify.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Settings_bg {
+            get {
+                object obj = ResourceManager.GetObject("Settings_bg", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap transaction_icon {
             get {
                 object obj = ResourceManager.GetObject("transaction_icon", resourceCulture);
